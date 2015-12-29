8c23a3c3da8581e07c35db837545cfa99ad559c9
Jerome Dockes <jerome@dockes.org>
2015-12-29 17:09:16 +0100

    change initialization, keep dirich parameters in non-log space, add verbose option to variational_inference

--- src/test.py
+++ src/test.py
@@ -13,30 +13,29 @@ path_to_reuters = path.expanduser(
 
 def test_variational_inference(voc = None, docs = None,
                                max_files = None, doc_num = None, n_topics = 30,
-                               log_dirich_param = None,
-                               log_word_proba_given_topic = None):
+                               dirich_param = .5,
+                               log_word_proba_given_topic = None,
+                               **kwargs):
 
     if(voc == None or docs == None):
         voc, docs = dp.build_voc(dp.find_reuters_files(path_to_reuters)[:max_files])
-        
+
     voc_size = len(voc)
 
     if doc_num == None:
         doc_count = len(docs)
         doc_num = np.random.randint(doc_count)
-        
-    if dirich_param == None:
 
-        log_dirich_param = 0
-
-    if word_proba_given_topic == None:
-        log_word_proba_given_topic = np.zeros((n_topics, voc_size)) \
-                                     - np.log(voc_size) 
+    if log_word_proba_given_topic == None:
+        word_proba_given_topic = np.random.rand(n_topics * voc_size).reshape(
+            (n_topics, voc_size))
+        word_proba_given_topic /= np.sum(word_proba_given_topic,
+                                         axis = 1).reshape((-1,1))
+        log_word_proba_given_topic = np.log(word_proba_given_topic)
 
     var_dirich, var_multinom, log_likelihoods = vi.variational_inference(
-        docs[doc_num], log_dirich_param, log_word_proba_given_topic,
-        save_log_likelihoods = True)
+        docs[doc_num], dirich_param, log_word_proba_given_topic, **kwargs)
 
     plt.plot(log_likelihoods)
     
-    vi.latent_dirichlet_allocation(docs, n_topics, voc_size)
+#    vi.latent_dirichlet_allocation(docs, n_topics, voc_size)
